821a4eab1ee4a8e799266f6b9c5f3e747687fdd3
Siddharth-Nama <siddharthnama2604@gmail.com>
2025-07-25 20:07:46 +0530

    Task 1: Design Database Schema (Backend: Django + DRF)

--- backendInventorySystem/Products/views.py
+++ backendInventorySystem/Products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.db import models
 
-# Create your views here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=100)
+    sku = models.CharField(max_length=100, unique=True)
+    image_url = models.URLField(blank=True, null=True)
+    description = models.TextField()
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - {self.type} - {self.sku}"
